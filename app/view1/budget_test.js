2fba4dbfe3098e50f0ce0bff68a7f84f824bf3b6
Angelo Veltens <angelo.veltens@online.de>
2014-10-18 13:10:45 +0200

    refactored test

--- app/view1/budget_test.js
+++ app/view1/budget_test.js
@@ -19,9 +19,11 @@ describe ('myApp.budget module', function () {
             scope.description = 'test transaction';
             scope.addTransaction();
             expect(scope.budget).toEqual(990);
-            expect(scope.transactions).toEqual([
-                {amount: -10, description: 'test transaction', date: new Date()}
-            ]);
+            expect(scope.transactions.length).toEqual(1);
+            var transaction = scope.transactions[0];
+            expect(transaction.amount).toEqual(-10);
+            expect(transaction.description).toEqual('test transaction');
+            expect(transaction.date - new Date()).toBeLessThan(1000) // date should differ less than 1 second
             expect(scope.amount).toBeNull();
             expect(scope.description).toBeNull();
         });
